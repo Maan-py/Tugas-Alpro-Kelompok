8068daf7a5f762ec32d4a271d8a7eacb3ffc76c5
MaanPy <11830227-MaanPy@users.noreply.replit.com>
2023-11-22 15:15:24 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@ProjectAlpro/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Projek Alpro" << endl;
+  cout << "Menu" << endl;
+  cout << "1. Bangun Dua Dimensi" << endl;
+  cout << "2. Bangun Tiga Dimensi" << endl;
+  cout << "3. Matriks" << endl;
+  cout << "4. Exit" << endl;
+}
