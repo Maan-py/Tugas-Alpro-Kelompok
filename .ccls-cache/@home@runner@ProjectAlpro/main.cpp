d9286b3471bf87e7d22a6db65bbe074d996cc1d3
Maan-py <96055301+Maan-py@users.noreply.github.com>
2023-11-22 21:54:40 +0000

    Nambah pilihan

--- .ccls-cache/@home@runner@ProjectAlpro/main.cpp
+++ .ccls-cache/@home@runner@ProjectAlpro/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int pilih;
+
 int main() {
   cout << "Projek Alpro" << endl;
   cout << "Menu" << endl;
   cout << "1. Bangun Dua Dimensi" << endl;
   cout << "2. Bangun Tiga Dimensi" << endl;
   cout << "3. Matriks" << endl;
-  cout << "4. Exit" << endl;
+  cout << "4. About" << endl;
+  cout << "5. Exit" << endl;
+
+  cout << "Pilihan : ";
+  cin >> pilih;
 }
