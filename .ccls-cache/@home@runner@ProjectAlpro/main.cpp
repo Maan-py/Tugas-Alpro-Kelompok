9891ee95622980f1ce35ad6b4c1a2ecc830ce867
mluqmaan23 <29492644-mluqmaan23@users.noreply.replit.com>
2023-11-27 07:12:41 +0000

    ngerjain menu 2

--- .ccls-cache/@home@runner@ProjectAlpro/main.cpp
+++ .ccls-cache/@home@runner@ProjectAlpro/main.cpp
@@ -5,7 +5,7 @@ int pilih;
 
 void menu1() {
   cout << "\033[0;0H\033[2J"; // alternatif system("cls");
-  
+
   cout << "1. Persegi Panjang" << endl;
   cout << "2. Persegi" << endl;
   cout << "3. Segitiga" << endl;
@@ -14,13 +14,46 @@ void menu1() {
   cout << "6. Belah Ketupat" << endl;
   cout << "7. Trapesium" << endl;
   cout << "8. Kembali ke menu utama" << endl;
-  
+
   cout << "\nPilih Menu : " << endl;
   cin >> pilih;
 
   switch(pilih) {
     case 1 :
+    break;
+    case 2 :
+    break;
+    case 3 :
+    break;
+    case 4 :
+    break;
+  }
+}
+
+void menu2() {
+  cout << "\033[0;0H\033[2J"; // alternatif system("cls");
+
+  cout << "1. Kubus" << endl;
+  cout << "2. Balok" << endl;
+  cout << "3. Prisma segitiga" << endl;
+  cout << "4. Tabung" << endl;
+  cout << "5. Bola" << endl;
+  cout << "6. Exit";
+
+  switch(pilih) {
+    case 1 :
       
+    break;
+    case 2 :
+    break;
+    case 3 :
+    break;
+    case 4 :
+    break;
+    case 5 :
+    break;
+    default :
+    cout << "Silakan pilih menu yang sesuai.";
     break;
   }
 }
@@ -42,12 +75,18 @@ int main() {
       menu1();
     break;
     case 2 :
+      // menu2();
     break;
     case 3 :
+      // menu3();
     break;
     case 4 :
+      // menu4();
     break;
     case 5 :
+    break;
+    default :
+      cout << "Silakan pilih menu yang sesuai.";
     break;
   }
 }
